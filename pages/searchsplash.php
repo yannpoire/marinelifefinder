88c42622e025187e480638891b61602053cc9f2a
Yann Poiré <y@nnpoire.com>
2015-05-18 11:55:00 +0700

    Changed splash appearance, try path for css

--- pages/searchsplash.php
+++ pages/searchsplash.php
@@ -1,6 +1,6 @@
 <?php
-	include_once 'common/header.php';
-	include_once 'common/mainnav.php';
+	include_once '../common/header.php';
+	include_once '../common/mainnav.php';
 ?>
 
 <div class="container">
@@ -8,56 +8,54 @@
 	<div class="row">
 		<h1>What are we looking for?</h1>
 		<p>Before we can proceed we need to know what kind of organism we are looking for. We can divide these organisms into 5 very distinc groups.</p>
-		<ul>
-			<li>
-				<span>Fishes</span>
-				<span>Regroups bony fishes which are the most common and non bony such as sharks and rays. Exclude marine mammals.</span>
-				<ul>
-					<li>Fishes</li>
-					<li>Sharks</li>
-					<li>Rays</li>
-				</ul>
-				</li>
-			<li>
-				<span>Crustaceas</span>
-				<span>With a hard shell</span>
-				<ul>
-					<li>Crabs</li>
-					<li>Shrimps</li>
-					<li>Lobsters</li>
-					<li>Prawns</li>
-				</ul>
-			</li>
-			<li>
-				<span>Encephalopods</span>
-				<span>Encephalopods means a head with arms and include octopuses and such.</span>
-				<ul>
-					<li>Octopuses</li>
-					<li>Squids</li>
-					<li>Cuttlefishes</li>
-				</ul>
-			</li>
-			<li>
-				<span>Corals</span>
-				<span>Corals are divided into 2 main families hard and soft corals.</span>
-				<ul>
-					<li>Hard corals</li>
-					<li>Soft corals</li>
-				</ul>
-			</li>
-			<li>
-				<span>Sponges</span>
-				<span>They feed by filtering the water around them. They play a major role keeping reefs clean.</span>
-			</li>
-			<li>
-				<span>Jellyfishes</span>
-			</li>
-			<li>
-				<span>Nudibranchs &amp; Slugs</span>
-			</li>
-		</ul>
+		<div class="col-md-4">
+			<h2>Fishes</h2>
+			<p>Regroups bony fishes which are the most common and non bony such as sharks and rays. Exclude marine mammals.</p>
+			<ul>
+				<li>Fishes</li>
+				<li>Sharks</li>
+				<li>Rays</li>
+			</ul>
+		</div>
+		<div class="col-md-4">
+			<h2>Crustaceas</h2>
+			<p>The ones with a hard shell</p>
+			<ul>
+				<li>Crabs</li>
+				<li>Shrimps</li>
+				<li>Lobsters</li>
+				<li>Prawns</li>
+			</ul>
+		</div>
+		<div class="col-md-4">
+			<h2>Encephalopods</span>
+			<p>Encephalopods means a head with arms and include octopuses and such.</p>
+			<ul>
+				<li>Octopuses</li>
+				<li>Squids</li>
+				<li>Cuttlefishes</li>
+			</ul>
+		</div>
+		<div class="col-md-4">
+			<h2>Corals</h2>
+			<p>Corals are divided into 2 main families hard and soft corals.</p>
+			<ul>
+				<li>Hard corals</li>
+				<li>Soft corals</li>
+			</ul>
+		</div>
+		<div class="col-md-4">
+			<h2>Sponges</h2>
+			<p>They feed by filtering the water around them. They play a major role keeping reefs clean.</p>
+		</div>
+		<div class="col-md-4">
+			<h2>Jellyfishes</h2>
+		</div>
+		<div class="col-md-4">
+			<h2>Nudibranchs &amp; Slugs</h2>
+		</div>
 	</div>
 	<hr>
 <div>
-	<?php include_once 'common/footer.php'; ?>
+	<?php include_once '../common/footer.php'; ?>
 </div>
