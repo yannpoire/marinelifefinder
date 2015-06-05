421b164bab5be99cc7d6e6a39e514e5e57a9536b
Yann Poiré <y@nnpoire.com>
2015-06-06 02:25:42 +0700

    Nightly

--- common/footer.php
+++ common/footer.php
@@ -7,7 +7,9 @@
 	</div>
 </div> <!-- /container -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="<?php echo BASE_URL; ?>js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+<script>window.jQuery || document.write('<script src="<?php echo BASE_URL; ?>js/vendor/jquery-2.1.4.min.js"><\/script>')</script>
+<script src="<?php echo BASE_URL; ?>js/vendor/jquery-ui-1.11.4/jquery-ui.min.js"></script>
 <script src="<?php echo BASE_URL; ?>js/vendor/bootstrap.min.js"></script>
 <script src="<?php echo BASE_URL; ?>js/plugins.js"></script>
 <script src="<?php echo BASE_URL; ?>js/main.js"></script>
